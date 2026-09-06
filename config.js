68368179979906d3300e782c28feeea8a7b989b0
gdauerkohut-svg <gdauerkohut@gmail.com>
2026-09-05 22:43:18 -0300

    config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ window.APP_CONFIG = {
   GOOGLE_CLIENT_ID: '575499197045-5nudmf8u156dg6r4g02vqjj37t13t4is.apps.googleusercontent.com',
 
   // URL de implantação do seu Apps Script, terminando em /exec.
-  // (Extensões → Apps Script → Implantar → Gerenciar implantações → copiar a URL do "App da Web")
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxp0fBVotS08X6QIXyMbGgHaJ_SXp0urm6NnnuKvcDOpUy6RvRnlkCB5xTzMY0JIotD/exec'
 };
