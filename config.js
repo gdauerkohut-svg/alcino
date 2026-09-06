c04d1940bbb03362144b2c3f31d8ea50749ae7db
gdauerkohut-svg <gdauerkohut@gmail.com>
2026-09-05 22:31:22 -0300

    config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@
  */
 window.APP_CONFIG = {
   // Client ID gerado no Google Cloud Console (Credenciais → OAuth Client ID → Web application).
-  GOOGLE_CLIENT_ID: 'COLE_AQUI_SEU_CLIENT_ID.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: 575499197045-5nudmf8u156dg6r4g02vqjj37t13t4is.apps.googleusercontent.com,
 
   // URL de implantação do seu Apps Script, terminando em /exec.
   // (Extensões → Apps Script → Implantar → Gerenciar implantações → copiar a URL do "App da Web")
-  APPS_SCRIPT_URL: 'COLE_AQUI_A_URL_DO_SEU_APPS_SCRIPT/exec'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxp0fBVotS08X6QIXyMbGgHaJ_SXp0urm6NnnuKvcDOpUy6RvRnlkCB5xTzMY0JIotD/exec'
 };
